e300d9af1a2c4185c1f82649204d711ca63b26e5
Andy Theuninck <andy@gohanman.com>
2017-12-03 12:52:10 -0600

    manhattan distance

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -21,6 +21,12 @@ fn down(p: &Point) -> Point {
     Point { x: p.x, y: p.y - 1 }
 }
 
+fn manhattan(a: &Point, b: &Point) -> i64 {
+    let x = a.x - b.x;
+    let y = a.y - b.y;
+    x.abs() + y.abs()
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -32,4 +38,5 @@ fn test() {
     assert_eq!(Point { x:1, y:0 }, right(&origin));
     assert_eq!(Point { x:0, y:1 }, up(&origin));
     assert_eq!(Point { x:0, y:-1 }, down(&origin));
+    assert_eq!(2, manhattan(&origin, &Point{ x: -1, y: 1 }));
 }
