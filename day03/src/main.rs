130a442c6bc9ad32ad40b2cd662072c9d1810d5b
Andy Theuninck <andy@gohanman.com>
2017-12-03 13:57:35 -0600

    Revise tests w/ answers

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -108,12 +108,9 @@ fn main() {
 
 #[test]
 fn test() {
-    let origin = Point { x:0, y:0 };
-    assert_eq!(Point { x:-1, y:0 }, left(&origin));
-    assert_eq!(Point { x:1, y:0 }, right(&origin));
-    assert_eq!(Point { x:0, y:1 }, up(&origin));
-    assert_eq!(Point { x:0, y:-1 }, down(&origin));
-    assert_eq!(2, manhattan(&origin, &Point{ x: -1, y: 1 }));
-    assert_eq!(Point { x:0, y:2 }, nth(15));
-    assert_eq!(Point { x:1, y:-2 }, nth(24));
+    let one = nth(325489);
+    let d = manhattan(&one, &Point { x:0, y:0 });
+    assert_eq!(552, d);
+    let two = more_than(325489);
+    assert_eq!(330785, two);
 }
